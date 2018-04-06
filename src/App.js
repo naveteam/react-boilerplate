db6393353928b6b9df80cb26367d33b19f0a36ac
jpbretanha <jpbretanha@Matheuss-Mac-mini.local>
2018-04-06 13:28:24 -0300

    Standard adicionado

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import { ConnectedRouter} from 'react-router-redux';
-import createHistory from 'history/createBrowserHistory';
-import Routes from './Routes';
-import createStore from './redux/store';
-import 'sanitize.css/sanitize.css';
+import React, { Component } from 'react'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
+import createHistory from 'history/createBrowserHistory'
+import Routes from './Routes'
+import createStore from './redux/store'
+import 'sanitize.css/sanitize.css'
 
-const history = createHistory();
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
-&& window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = createStore(history, devTools);
+const history = createHistory()
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ &&
+window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = createStore(history, devTools)
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <Provider store={store}>
         <ConnectedRouter history={history}>
           <Routes />
         </ConnectedRouter>
       </Provider>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/Routes.js
+++ src/Routes.js
@@ -4,11 +4,11 @@ import {
   BrowserRouter as Router
 } from 'react-router-dom'
 
-export default () =>  (
-    <Router>
-        <Fragment>
-            <Route exact path="/" component={() => (<div>Rota  1</div>)}/>
-            <Route path="/about" component={() => (<div>Rota 2</div>)}/>
-        </Fragment>
-    </Router>
+export default () => (
+  <Router>
+    <Fragment>
+      <Route exact path='/' component={() => (<div>Rota  1</div>)} />
+      <Route path='/about' component={() => (<div>Rota 2</div>)} />
+    </Fragment>
+  </Router>
 )
