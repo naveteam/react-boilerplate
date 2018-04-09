61f334a7482adbb83a25275fe346a36c2f7390ab
jpbretanha <jpbretanha@Matheuss-Mac-mini.local>
2018-04-09 15:17:39 -0300

    Novo exemplo de uso de rotas

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,14 @@ import {
   Route,
   BrowserRouter as Router
 } from 'react-router-dom'
+import Rota1 from './components/Rota1'
+import Rota2 from './components/Rota2'
 
 export default () => (
   <Router>
     <Fragment>
-      <Route exact path='/' component={() => (<div>Rota  1</div>)} />
-      <Route path='/about' component={() => (<div>Rota 2</div>)} />
+      <Route exact path='/' component={Rota1} />
+      <Route path='/rota2' component={Rota2} />
     </Fragment>
   </Router>
 )
---
+++ src/components/Rota2.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default () => (
+  <Link to='/'>rota 1</Link>
+)
