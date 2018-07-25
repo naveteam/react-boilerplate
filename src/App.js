6cea0b64d50dde48e2c221ca26e090e1fe4f3039
Joao Pedro Bretanha <jpbretanha@gmail.com>
2018-07-25 11:16:42 -0300

    refactoring boilerplate and redux config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React, { Component } from 'react'
-import { Provider } from 'react-redux'
-import { ConnectedRouter } from 'react-router-redux'
-import createHistory from 'history/createBrowserHistory'
+// import { Provider } from 'react-redux'
+// import createHistory from 'history/createBrowserHistory'
 import Routes from './Routes'
-import createStore from './redux/store'
+// import store from './redux/store'
 import 'sanitize.css/sanitize.css'
-
-const history = createHistory()
-const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store = createStore(history, devTools)
 
 class App extends Component {
   render () {
     return (
-      <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <Routes />
-        </ConnectedRouter>
-      </Provider>
+      // <Provider store={store}>
+      <Routes />
+      // </Provider>
     )
   }
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,13 @@
-import { createStore as _createStore, applyMiddleware } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import { routerMiddleware } from 'react-router-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
 import reducer from './modules'
 
-export default function createStore (history, data) {
-  const reduxRouterMiddleware = routerMiddleware(history)
-  const middleware = [thunkMiddleware, reduxRouterMiddleware]
-
-  const finalCreateStore = applyMiddleware(...middleware)(_createStore)
-  const store = finalCreateStore(reducer, data)
-
-  return store
+const middleware = [thunk]
+if (process.env.NODE_ENV === 'development') {
+  middleware.push(logger)
 }
+
+const store = createStore(reducer, applyMiddleware(...middleware))
+
+export default store
