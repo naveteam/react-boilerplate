e5a3cf648ddb42d62acf965af6de995f6dfdb2f0
Joao Pedro Bretanha <jpbretanha@gmail.com>
2018-12-20 15:28:01 -0200

    new fetchClient config

--- src/providers/fetchClient.js
+++ src/providers/fetchClient.js
@@ -2,31 +2,33 @@ import axios from 'axios'
 
 export const __API__ = ''
 
-const fetchClient = () => {
-  const defaultOptions = {
-    baseURL: __API__
-  }
+const defaultOptions = {
+  baseURL: __API__
+}
 
-  let instance = axios.create(defaultOptions)
+let instance = axios.create(defaultOptions)
 
-  instance.interceptors.request.use(config => {
-    /* global localStorage */
-    config.headers.Authorization = localStorage.getItem('token')
-    return config
-  })
+instance.interceptors.request.use(config => {
+  /* global localStorage */
+  config.headers.Authorization = localStorage.getItem('token')
+  return config
+})
 
-  instance.interceptors.response.use(function (response) {
+instance.interceptors.response.use(
+  function (response) {
     return response
-  }, error => {
-    if (error.response.status === 401) {
-      // logica de redirect aqui
-      window.location('/login')
+  },
+  error => {
+    if (
+      error &&
+      error.response &&
+      error.response.status === 401 &&
+      window.location.pathname !== '/login'
+    ) {
+      window.location.href = '/login'
     } else {
       return Promise.reject(error)
     }
-  })
-
-  return instance
-}
-
-export default fetchClient()
+  }
+)
+export default instance
