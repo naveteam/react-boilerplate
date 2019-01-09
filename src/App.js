a10f4790114eec25db69c45e36af1b6a349684a5
Joao Pedro Bretanha <jpbretanha@gmail.com>
2019-01-09 20:23:50 -0200

    update packages used and folder structure

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 // import { Provider } from 'react-redux'
 // import createHistory from 'history/createBrowserHistory'
-import Routes from './Routes'
 // import store from './redux/store'
 import 'sanitize.css/sanitize.css'
+import Rota1 from './routes/Rota1'
+import Rota2 from './routes/Rota2'
 
 class App extends Component {
   render () {
     return (
       // <Provider store={store}>
-      <Routes />
+      <Router>
+        <Fragment>
+          <Route exact path='/' component={Rota1} />
+          <Route path='/rota2' component={Rota2} />
+        </Fragment>
+      </Router>
       // </Provider>
     )
   }
--- src/redux/modules/index.js
+++ src/redux/modules/index.js
@@ -1,6 +1,3 @@
 import { combineReducers } from 'redux'
-import { routerReducer } from 'react-router-redux'
 
-export default combineReducers({
-  routing: routerReducer
-})
+export default combineReducers({})
