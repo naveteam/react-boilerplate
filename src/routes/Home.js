4c0945fa4c7482cd44239aee8588835f43b2209f
Joao Pedro Bretanha <jpbretanha@gmail.com>
2019-08-27 14:41:19 -0300

    chore(eslint+prettier): configure eslint + prettier + stylelint

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default () => <Link to='/dashboard'>GO TO DASHBOARD</Link>
+export default () => {
+  return <Link to='/dashboard'>GO TO DASHBOARD</Link>
+}
