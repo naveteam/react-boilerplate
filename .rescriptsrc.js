1567762c9755373eb4bead106dda49499185a857
João Pedro Bretanha <joaopedro@nave.rs>
2019-10-04 14:13:41 -0300

    fix(build): fix yarn build script (#6)
    * fix(build): fix yarn build script
    
    fix spread operator missing in array breaking build and add a fix on prettier script in package.json
    
    * fix(git): fix prettier script adding all files to commit

--- .rescriptsrc.js
+++ .rescriptsrc.js
@@ -1,6 +1,6 @@
 const commonPlugins = ["@babel/plugin-proposal-optional-chaining"]
 const productionPlugins = [['transform-remove-console', { exclude: ['error', 'warn'] }]]
-const plugins = process.env.NODE_ENV === 'production' ? [...commonPlugins, productionPlugins] : [...commonPlugins]
+const plugins = process.env.NODE_ENV === 'production' ? [...commonPlugins, ...productionPlugins] : [...commonPlugins]
 
 module.exports = [
   ['use-babel-config',
