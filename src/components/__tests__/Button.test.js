c72d8e6bf9f2b45f44574abe4a2fa205fad073a4
gabriel <gabriel.couto@nave.rs>
2020-02-12 01:03:32 -0300

    test: migrates enzyme to rtl

--- src/components/__tests__/Button.test.js
+++ src/components/__tests__/Button.test.js
@@ -1,4 +1,3 @@
-import { shallow } from 'enzyme'
 import React from 'react'
 import renderer from 'react-test-renderer'
 import Button from '../Button'
@@ -8,13 +7,5 @@ describe('<Button />', () => {
     const component = renderer.create(<Button disabled />)
     const tree = component.toJSON()
     expect(tree).toMatchSnapshot()
-  })
-
-  it('testing click', () => {
-    /* global jest */
-    const handler = jest.fn()
-    const button = shallow(<Button onClick={handler} />)
-    button.find('button').simulate('click')
-    expect(handler.mock.calls.length).toEqual(0)
   })
 })
