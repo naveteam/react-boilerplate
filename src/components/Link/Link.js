7febc45c5ccfb2f876bb3dedc9b6b0e5484515e2
Gustavo Pinho <gustavo@nave.rs>
2020-04-14 22:33:55 -0300

    feat(link): create link component

---
+++ src/components/Link/Link.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+import Text from 'components/Text'
+
+const getProps = ({ to, textVariant, ...props }) => {
+  if (to) {
+    return {
+      to,
+      as: Link,
+      ...props
+    }
+  }
+
+  return {
+    as: 'a',
+    ...props
+  }
+}
+
+const LinkComponent = props => <LinkText {...getProps(props)} />
+
+const LinkText = styled(Text)`
+  text-decoration: ${({ textDecoration }) => textDecoration};
+`
+
+LinkComponent.propTypes = {
+  textDecoration: PropTypes.string,
+  to: PropTypes.string,
+  href: PropTypes.string
+}
+
+export default LinkComponent
