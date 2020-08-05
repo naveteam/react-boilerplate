8f5db173e07b22b5e91b1b81cd365c816758e248
Gabriel Do Couto Santos <lc.gabrieldocouto@outlook.com>
2020-08-05 19:20:27 -0300

    feat: new services implementation

--- src/services/auth.js
+++ src/services/auth.js
@@ -2,4 +2,7 @@ import client from 'providers/fetchClient'
 
 export const getUser = () => client.get('/v1/me')
 
-export const login = data => client.post('/v1/users/login', data)
+export const login = async data => {
+  const { token, ...user } = await client.post('/v1/users/login', data)
+  return { token, user }
+}
