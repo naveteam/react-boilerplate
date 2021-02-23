77837d0a3ed6d1b102640b405405ad4ae54329f2
rbschumacher <rafael.schumacher@nave.rs>
2021-02-23 09:28:44 -0300

    feat: remove apollo rest client

--- src/providers/apolloClient.js
+++ src/providers/apolloClient.js
@@ -1,10 +1,8 @@
 import { ApolloClient, InMemoryCache, HttpLink, ApolloLink } from '@apollo/client'
-import { RestLink } from 'apollo-link-rest'
 import { onError } from '@apollo/client/link/error'
 
 import { getToken } from 'helpers'
 
-// const restLink = new RestLink({ uri: process.env.REACT_APP_API_URL }) // link for rest requests
 const httpLink = new HttpLink({ uri: process.env.REACT_APP_API_URL }) // link for graphQl requests
 
 const authMiddleware = new ApolloLink((operation, forward) => {
@@ -37,7 +35,6 @@ const errorMiddleware = onError(({ graphQLErrors, networkError }) => {
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: ApolloLink.from([authMiddleware, errorMiddleware, httpLink])
-  // link: ApolloLink.from([authMiddleware, errorMiddleware, restLink])
 })
 
 export default client
